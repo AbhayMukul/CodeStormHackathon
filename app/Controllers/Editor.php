24b901e73fb0bb925f7d33b68af28f7506fbd7ed
JadenFurtado <52862591+JadenFurtado@users.noreply.github.com>
2021-09-24 10:20:09 +0530

    Pushing the updates

--- app/Controllers/Editor.php
+++ app/Controllers/Editor.php
@@ -22,4 +22,23 @@ class Editor extends BaseController
 		return view('index',$data);
 	}	
 
+	public function html_css(){
+		//$data = "";
+		//return view("html_css_js",$data);
+		if(!isset($_GET['Challange_type'])&&!isset($_GET['challange_id'])){
+			$data['code']='<pre></pre>';
+		}
+		else{
+			//check if file exists
+			if (file_exists($_SERVER['DOCUMENT_ROOT'].'/code_editor/code/'.$_GET['challange_type'].'/user_id/'.$_GET['challange_id'].'.html')){
+      			$data['code']="<pre>".file_get_contents($_SERVER['DOCUMENT_ROOT'].'/code_editor/code/'.$_GET['challange_type'].'/user_id/'.$_GET['challange_id'].'.html')."</pre>";
+			}
+			else{
+				$data['code']="<pre>file does not exist!</pre>";				
+			}
+		}
+		//$data['code'] = '<pre></pre>';
+		return view("html_css_js",$data);
+	} 
+
 }
